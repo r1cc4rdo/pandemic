1ab927be3c23f63d0278ad757b3002b6b3d5cd4d
Riccardo Gherardi <riccardo.gherardi@gmail.com>
2020-05-23 02:10:25 -0700

    Add README. PEP8 formatting

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# pandemic
-Pandemic role chooser
+# [Pandemic role chooser](https://repl.it/@riccardog/Pandemic)
+[Pandemic](https://en.wikipedia.org/wiki/Pandemic_(board_game)) is a board
+game from [Z-MAN games](https://www.zmangames.com/en/games/pandemic/).
+
+Run **[the role chooser tool online on repl.it](https://repl.it/@riccardog/Pandemic)**!
+
+### More info
+
+* [Wikipedia entry](https://en.wikipedia.org/wiki/Pandemic_(board_game))
+* [On BoardGameGeek](https://boardgamegeek.com/boardgame/30549/pandemic)
+* [On Steam (videogame)](https://store.steampowered.com/app/622440/Pandemic_The_Board_Game/)
+* [Z-MAN games website](https://www.zmangames.com/en/games/pandemic/)
--- pandemic.py
+++ pandemic.py
@@ -1,19 +1,45 @@
 import random
 
 roles_db = {
-    'Researcher': "Can GIVE any card to a player in the same location. The card can be shared in either player's turn. Standard rules apply for RECEIVING cards.",
-    'Scientist': "Needs only 4 City cards (normally 5) of the same color to Discover a Cure.",
-    'Medic': "Removes all cubes of the chosen color when Treating Disease. Automatically removes all cubes of cured diseases from current location, even outside of his turn.",
-    'Dispatcher': "For 1 action, can move any pawn to any city containing another pawn. Can, with permission, MOVE other players' pawns using his own action points and city cards. Does NOT use players' cards for direct/charter flights.",
-    'Operations Expert': "Can build a research station in the current city without having/using a card. Once per turn, can move anywhere from a research station by discarding any city card.",
-    'Contingency Planner': "For 1 action, recovers an Event card from the Discard Pile. The card is not counted as part of the hand. Only 1 Event card can be set aside at a time.",
-    'Quarantine Specialist': "Prevents the placement of cubes in their current city and those connected to it.",
-    'Field Operative': "Once per turn, for 1 action, can remove and store 1 cube from the city they are in. Can discover a Cure with three cards and three stored cubes of the same color.",
-    'Generalist': "Can perform up to 5 actions per turn, instead of the usual 4.",
-    'Containment Specialist': "When entering a city with 2+ cubes of the same color, removes 1 cube for that disease. If multiple diseases have 2+ cubes, removes 1 from each.",
-    'Epidemiologist': "Once per turn, can take any City card from another player in the same city. The ability does not consume action points.",
-    'Archivist': "Can hold up to 8 City cards (normal limit: 7). Once per turn, for 1 action, recovers the City card that matches their location from the Discard Pile.",
-    'Troubleshooter': "Can look at the top N cards of the Infection deck, where N is the current Infection Rate. Keeps the card when flying TO a city in their hand (Direct Flight)."}
+    'Researcher':
+        "Can GIVE any card to a player in the same location. "
+        "The card can be shared in either player's turn. "
+        "Standard rules apply for RECEIVING cards.",
+    'Scientist':
+        "Needs only 4 City cards (normally 5) of the same color to Discover a Cure.",
+    'Medic':
+        "Removes all cubes of the chosen color when Treating Disease. "
+        "Automatically removes all cubes of cured diseases from current location, even outside of his turn.",
+    'Dispatcher':
+        "For 1 action, can move any pawn to any city containing another pawn. "
+        "With permission, can MOVE other players' pawns using his own action points and city cards. "
+        "Does NOT use players' cards for direct/charter flights.",
+    'Operations Expert':
+        "Can build a research station in the current city without having/using a card. "
+        "Once per turn, can move anywhere from a research station by discarding any city card.",
+    'Contingency Planner':
+        "For 1 action, recovers an Event card from the Discard Pile. "
+        "The card is not counted as part of the hand. "
+        "Only 1 Event card can be set aside at a time.",
+    'Quarantine Specialist':
+        "Prevents the placement of cubes in their current city and those connected to it.",
+    'Field Operative':
+        "Once per turn, for 1 action, can remove and store 1 cube from the city they are in. "
+        "Can discover a Cure with three cards and three stored cubes of the same color.",
+    'Generalist':
+        "Can perform up to 5 actions per turn, instead of the usual 4.",
+    'Containment Specialist':
+        "When entering a city with 2+ cubes of the same color, removes 1 cube for that disease. "
+        "If multiple diseases have 2+ cubes, removes 1 from each.",
+    'Epidemiologist':
+        "Once per turn, can take any City card from another player in the same city. "
+        "The ability does not consume action points.",
+    'Archivist':
+        "Can hold up to 8 City cards (normal limit: 7). "
+        "Once per turn, for 1 action, recovers the City card that matches their location from the Discard Pile.",
+    'Troubleshooter':
+        "Can look at the top N cards of the Infection deck, where N is the current Infection Rate. "
+        "Keeps the card when flying TO a city in their hand (Direct Flight)."}
 
 
 def print_role(role, player=None):
@@ -29,7 +55,7 @@ def print_all_roles():
 def random_roles(players, randomize_order, base_roles_only, assign_roles):
     players = players.split()  
     if randomize_order:
-      random.shuffle(players)
+        random.shuffle(players)
 
     roles = list(roles_db)
     roles = roles[:7] if base_roles_only else roles
@@ -38,8 +64,9 @@ def random_roles(players, randomize_order, base_roles_only, assign_roles):
     for player, role in zip(players, roles):
         print_role(role, player if assign_roles else None)
 
+
 if __name__ == '__main__':
     random_roles('Yuko Riccardo Audrey German',
-        randomize_order=True,  # randomize players' order
-        base_roles_only=False,  # don't use roles from the 'On the Brink' extension
-        assign_roles=True)  # assign roles to players, instead of letting them pick
+                 randomize_order=True,  # randomize players' order
+                 base_roles_only=False,  # don't use roles from the 'On the Brink' extension
+                 assign_roles=True)  # assign roles to players, instead of letting them pick
